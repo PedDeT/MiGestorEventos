9355c342b722f6dcd8a66fc9c235f40b83fdc3be
peddet <pldelatorre@hotmail.com>
2024-09-11 11:43:22 -0500

    cambios usando django allauth

--- eventos/urls.py
+++ eventos/urls.py
@@ -2,9 +2,7 @@ from django.urls import path
 from . import views
 
 
-urlpatterns = [
-    path("", views.VistaListaEventos.as_view(), name="lista_eventos"),
-    path("organizador/", views.VistaListaOrganizador.as_view(), name="lista_organizador"),
-    path("organizador/new/", views.VistaCrearOrganizador.as_view(), name="crear_organizador"),
-    path("evento/<int:pk>/edit/", views.VistaEditarEvento.as_view(), name="editar_evento"),
+app_name = 'eventos'
+urlpatterns = [path("", views.Home.as_view(), name="home"),
+               path("crear_evento", views.CrearEvento.as_view(), name="crear_evento"),
 ]
